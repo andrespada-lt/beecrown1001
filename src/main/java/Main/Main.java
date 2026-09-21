e932b9724d9ce5a348122a11ac40bdb372f19370
1072521300 <1072521300@UNS07DT99136.animaedu.intranet>
2026-09-21 19:37:19 -0300

    https://github.com/andrespada-lt/beecrown1001.git

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+       int A, B, X;
+       Scanner Leitor = new Scanner(System.in);
+       A = Leitor.nextInt();
+       B = Leitor.nextInt();
+       X = A + B;
+       System.out.println("X = " + X);
+    }
+}
